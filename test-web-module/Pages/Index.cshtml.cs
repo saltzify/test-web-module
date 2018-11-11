985e95b3c5fd638414553af18bc02426b04cd842
saltzify <saltzify@gmail.com>
2018-11-10 16:28:11 -0800

    initial commit

---
+++ test-web-module/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace test_web_module.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
